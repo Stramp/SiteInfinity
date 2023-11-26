5ddea840d92e2a65ebd6194131c0df04fa927b81
rafael <rafael.macedo@infracommerce.com.br>
2023-11-26 16:23:22 -0300

    fix conflict var

--- src/components/Teste/styles.ts
+++ src/components/Teste/styles.ts
@@ -4,8 +4,8 @@ export const Box = styled.main`
   ${({ theme }) => css`
     width: 100%;
     height: 100%;
-    background-color: ${theme.colors.black};
-    color: ${theme.colors.white};
+    background-color: ${theme.colors.invertBg};
+    color: ${theme.colors.primary};
     display: flex;
     align-items: center;
     justify-content: center;
